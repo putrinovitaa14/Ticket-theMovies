a2d559278c5cc069cba62892fd82c2c4ecc0e0dc
PLABS.ID <plabs@PLABSIDs-MacBook-Pro.local>
2022-11-28 16:01:28 +0700

    T-movies

---
+++ tmovies_project/tmovies-ui/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import MoviePage from "./pages/Movies";
+import Player from "./pages/Player";
+import Signup from "./pages/Signup";
+import Tmovies from "./pages/Tmovies";
+import TVShows from "./pages/TVShows";
+import UserListedMovies from "./pages/UserListedMovies";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/player" element={<Player />} />
+        <Route exact path="/tv" element={<TVShows />} />
+        <Route exact path="/movies" element={<MoviePage />} />
+        <Route exact path="/new" element={<Player />} />
+        <Route exact path="/mylist" element={<UserListedMovies />} />
+        <Route exact path="/" element={<Tmovies />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
